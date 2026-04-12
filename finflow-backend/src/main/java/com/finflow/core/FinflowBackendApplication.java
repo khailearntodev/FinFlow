376bb50d449b6b5bd65986291a3c295ed679062b
Nguyen Quang Khai <khainq205@gmail.com>
2026-04-13 01:13:13 +0700

    feat: initialize spring boot backend using GraphQL, Docker

---
+++ finflow-backend/src/main/java/com/finflow/core/FinflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finflow.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinflowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinflowBackendApplication.class, args);
+    }
+
+}
---
+++ finflow-backend/src/test/java/com/finflow/core/FinflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finflow.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinflowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
